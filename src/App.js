76bb5abe44c4d4eb1cb17220fe3f64557a19d4c9
Yannis <yannis.baltus@outlook.com>
2021-07-07 21:41:33 +0200

    Rendering a list of data thanks to a filter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import React, {useState} from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+
+const DUMMY_EXPENSES = [
+    {id: 'e1', title: 'Toilet paper', amount:'94.12', date: new Date(2020,11,15)},
+    {id: 'e2', title: 'Car insurance', amount:'297.64', date: new Date(2021,2,28)},
+    {id: 'e3', title: 'New Samsung TV', amount:'599.99', date: new Date(2019,5,1)},
+    {id: 'e4', title: 'New desk', amount:'352.13', date: new Date(2021,6,5)}
+];
+
 const App = () => {
-    const expenses = [
-        {id: 'e1', title: 'Toilet paper', amount:'94.12', date: new Date(2020,11,15)},
-        {id: 'e2', title: 'Car insurance', amount:'297.64', date: new Date(2021,2,28)},
-        {id: 'e3', title: 'New Samsung TV', amount:'599.99', date: new Date(2019,5,1)},
-        {id: 'e4', title: 'New desk', amount:'352.13', date: new Date(2021,6,5)},
-    ];
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
     const addExpenseHandler = (expense) => {
-        console.log('In app js');
-        console.log(expense);
-    }
+        setExpenses((prevExpenses) => {
+            return [expense, ...prevExpenses];
+        });
+    };
 
   return (
       <div>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,37 +4,35 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import React, { useState } from "react";
 
-const Expenses = ({items}) => {
-    const [filteredYear, setFilteredYear] = useState('2020');
+const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2022');
 
     const expenseFilterHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     };
 
+    const filteredExpenses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    let expensesContent = (<p>No expenses found.</p>);
+
+    if(filteredExpenses.length > 0) {
+        expensesContent = (filteredExpenses.map((expense) => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+        )));
+    }
+
     return (
         <div>
             <Card className={"expenses"}>
                 <ExpensesFilter selected={filteredYear} onYearSelected={expenseFilterHandler}/>
-                <ExpenseItem
-                    title={items[0].title}
-                    amount={items[0].amount}
-                    date={items[0].date}
-                />
-                <ExpenseItem
-                    title={items[1].title}
-                    amount={items[1].amount}
-                    date={items[1].date}
-                />
-                <ExpenseItem
-                    title={items[2].title}
-                    amount={items[2].amount}
-                    date={items[2].date}
-                />
-                <ExpenseItem
-                    title={items[3].title}
-                    amount={items[3].amount}
-                    date={items[3].date}
-                />
+                {expensesContent}
             </Card>
         </div>
     )
